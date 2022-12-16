70ba7ffde32f55e2f2852149c17239f693562268
Andy Kleindienst <andy.kleindienst@gmail.com>
2022-12-16 11:25:22 -0500

    added timer display

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
-var timerDisplay = document.createElement('h2')
-var secondsLeft = 60;
 
-function setTime() {
-    var timerInt = setInterval(function () {
+var secondsLeft = 60;
+var testKnowledge = document.getElementById('testKnowledge');
+var startButton = document.getElementById('start-button');
+var timerInterval = '';
+
+function setTimer() {
+    secondsLeft = 60;
+    timerInterval = setInterval(function () {
         secondsLeft--;
-        timerDisplay = secondsLeft;
+        testKnowledge.innerText = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            testKnowledge.innerText = "Whoa, dude! Better Study More!"
+        }
     }, 1000)
 }
+
+
+
+startButton.addEventListener("click", setTimer);
+
+
+// TODO turn the start button into the stop button when it is pressed after initializing
