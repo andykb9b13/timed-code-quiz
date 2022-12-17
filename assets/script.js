cad5a4280d2a45cc902918c1d6b42eab40a62027
Andy Kleindienst <andy.kleindienst@gmail.com>
2022-12-17 10:08:31 -0500

    put questions ionto an array

--- assets/script.js
+++ assets/script.js
@@ -34,32 +34,32 @@ function toggleButton() {
 // TODO A next button will show up on the screen
 // TODO There needs to be a counter on the screen for current wins and high score
 // TODO make another JS file with the answers and questions in them to be referenced
-var question1 = {
-    question: "Which of the following is not a primitive type in JavaScript?",
-    choice1: "String",
-    choice2: "Boolean",
-    choice3: "Script",
-    choice4: "Null",
-    answer: "Script"
-}
-
-var question2 = {
-    question: "Which of the following is the right syntax for CSS?",
-    choice1: "color: 'blue';",
-    choice2: "'background': white;",
-    choice3: "font-family = serif;",
-    choice4: "'line-height': '10px';",
-    answer: "color: 'blue';"
-}
-
-var question3 = {
-    question: "Which of the following moves elements along the main axis in CSS?",
-    choice1: "justify-items",
-    choice2: "align-content",
-    choice3: "center-content",
-    choice4: "justify-content",
-    answer: "justify-content"
-}
+let questions = [
+    {
+        question: "Which of the following is not a primitive type in JavaScript?",
+        choice1: "String",
+        choice2: "Boolean",
+        choice3: "Script",
+        choice4: "Null",
+        answer: "Script"
+    },
+    {
+        question: "Which of the following is the right syntax for CSS?",
+        choice1: "color: 'blue';",
+        choice2: "'background': white;",
+        choice3: "font-family = serif;",
+        choice4: "'line-height': '10px';",
+        answer: "color: 'blue';"
+    },
+    {
+        question: "Which of the following moves elements along the main axis in CSS?",
+        choice1: "justify-items",
+        choice2: "align-content",
+        choice3: "center-content",
+        choice4: "justify-content",
+        answer: "justify-content"
+    }
+];
 
 var choice1 = document.querySelector("#choice1");
 var choice2 = document.querySelector("#choice2");
@@ -67,15 +67,17 @@ var choice3 = document.querySelector("#choice3");
 var choice4 = document.querySelector("#choice4");
 var guessForm = document.querySelector(".question-card")
 var displayAnswer = document.createElement('p')
-var realAnswer = question1.answer;
+var realAnswer = questions[0].answer;
 var nextButton = document.querySelector("#next-button")
 var score = 0;
-choice1.innerText = question1.choice1;
-choice2.innerText = question1.choice2;
-choice3.innerText = question1.choice3;
-choice4.innerText = question1.choice4;
+choice1.innerText = questions[0].choice1;
+choice2.innerText = questions[0].choice2;
+choice3.innerText = questions[0].choice3;
+choice4.innerText = questions[0].choice4;
 
 nextButton.style.display = "none"
+
+
 
 function rightAnswer() {
     displayAnswer.innerText = "CORRECT!";
@@ -91,7 +93,7 @@ function wrongAnswer() {
 }
 
 function checkAnswer1() {
-    if (question1.choice1 === realAnswer) {
+    if (questions[0].choice1 === realAnswer) {
         rightAnswer()
     } else {
         wrongAnswer()
@@ -99,7 +101,7 @@ function checkAnswer1() {
 }
 
 function checkAnswer2() {
-    if (question1.choice2 === realAnswer) {
+    if (questions[0].choice2 === realAnswer) {
         rightAnswer()
     } else {
         wrongAnswer()
@@ -107,7 +109,7 @@ function checkAnswer2() {
 }
 
 function checkAnswer3() {
-    if (question1.choice3 === realAnswer) {
+    if (questions[0].choice3 === realAnswer) {
         rightAnswer()
     } else {
         wrongAnswer()
@@ -115,7 +117,7 @@ function checkAnswer3() {
 }
 
 function checkAnswer4() {
-    if (question1.choice4 === realAnswer) {
+    if (questions[0].choice4 === realAnswer) {
         rightAnswer()
     } else {
         wrongAnswer()
@@ -129,8 +131,10 @@ choice3.addEventListener("click", checkAnswer3);
 choice4.addEventListener("click", checkAnswer4);
 
 function nextQuestion() {
-
+    // need to add 1 to the array index each time the next button is pressed. 
+    // maybe make a random selector if you have time.
 }
 
 // nextButton.addEventListener("click",)
+
 
