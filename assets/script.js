80b2df42e1e796bb2ae7c3b65dc3efcaabe80697
Andy Kleindienst <andy.kleindienst@gmail.com>
2022-12-20 13:25:29 -0500

    added css styling for champions list

--- assets/script.js
+++ assets/script.js
@@ -58,150 +58,150 @@ let questions = [
         choice4: "Document Object Model",
         answer: "Document Object Model"
     },
-    // {
-    //     question: "Which is the most efficient algorithm using 'Big O' notation?",
-    //     choice1: "O(N)",
-    //     choice2: "O(1)",
-    //     choice3: "O(LogN)",
-    //     choice4: "O(2N)",
-    //     answer: "O(1)"
-    // },
-    // {
-    //     question: "Which is the correct way to identify 'orange' in the following array: var fruits = ['apple', 'orange', 'pear];",
-    //     choice1: "fruits[1]",
-    //     choice2: "fruits[0]",
-    //     choice3: "fruits[2]",
-    //     choice4: "fruits[orange]",
-    //     answer: "fruits[1]"
-    // },
-    // {
-    //     question: "Which of the following is the right syntax for calling an element with the class name 'first",
-    //     choice1: "document.querySelector(.first)",
-    //     choice2: "document.querySelector('.first')",
-    //     choice3: "document.getClassName(.first)",
-    //     choice4: "document.getClassName('.first')",
-    //     answer: "document.querySelector('.first')"
-    // },
-    // {
-    //     question: "Which best describes JavaScript?",
-    //     choice1: "double-threaded",
-    //     choice2: "triple-threaded",
-    //     choice3: "non-threaded",
-    //     choice4: "single-threaded",
-    //     answer: "single-threaded"
-    // },
-    // {
-    //     question: "Which variable's value cannot be changed once declared?",
-    //     choice1: "const",
-    //     choice2: "let",
-    //     choice3: "var",
-    //     choice4: "span",
-    //     answer: "const"
-    // },
-    // {
-    //     question: "Which method extracts a portion of a string without altering the original and returns it as a new string?",
-    //     choice1: "trim()",
-    //     choice2: "replace()",
-    //     choice3: "slice()",
-    //     choice4: "splice()",
-    //     answer: "slice()"
-    // },
-    // {
-    //     question: "Which method removes the decimal from a number?",
-    //     choice1: "Math.random()",
-    //     choice2: "console.log()",
-    //     choice3: "Math.floor()",
-    //     choice4: "indexOf()",
-    //     answer: "Math.floor()"
-    // },
-    // {
-    //     question: "How many times will this loop run? for (let i = 0; i < 5; i++)",
-    //     choice1: "3",
-    //     choice2: "10",
-    //     choice3: "15",
-    //     choice4: "5",
-    //     answer: "5"
-    // },
-    // {
-    //     question: "Which of the following will create an infinite loop?",
-    //     choice1: "for (let i = 0; i < 6; i++)",
-    //     choice2: "for (let i = 0; i > 5; i++)",
-    //     choice3: "for (let i = 6; i > 5; i++)",
-    //     choice4: "for (let i = 4; i > 0; i--)",
-    //     answer: "for (let i = 6; i > 5; i++)"
-    // },
-    // {
-    //     question: "Which of the following is the conditon for the loop? for (let i = 0; i < 5; i++)",
-    //     choice1: "i < 5",
-    //     choice2: "let i = 0",
-    //     choice3: "i++",
-    //     choice4: "for",
-    //     answer: "i < 5"
-    // },
-    // {
-    //     question: "Which of the following is true?",
-    //     choice1: "5 === '5'",
-    //     choice2: "2 != '2'",
-    //     choice3: "true !== false",
-    //     choice4: "5 == 'five'",
-    //     answer: "2 != '2'"
-    // },
-    // {
-    //     question: "Which of the following is NOT correct syntax for writing a function?",
-    //     choice1: "function newFunc() { console.log('hello!) }",
-    //     choice2: "var newFunc = function() { console.log('hello!) }",
-    //     choice3: "var function() = newFunc { console.log('hello') }",
-    //     choice4: "var newFunc = () => { console.log('hello') }",
-    //     answer: "var function() = newFunc { console.log('hello') }"
-    // },
-    // {
-    //     question: "For the variable 'let x=5', which of the following will satisfy the conditions?",
-    //     choice1: "if (x > 1 && x < 4)",
-    //     choice2: "if (x > 1 || x < 4)",
-    //     choice3: "if (x < 1 || x < 4)",
-    //     choice4: "if (x > 5 && x < 10)",
-    //     answer: "if (x > 1 || x < 4)"
-    // },
-    // {
-    //     question: "Which of the following means 'Not A Number'?",
-    //     choice1: "undefined",
-    //     choice2: "NaN",
-    //     choice3: "null",
-    //     choice4: "false",
-    //     answer: "NaN"
-    // },
-    // {
-    //     question: "Which of the following will return NaN when called?",
-    //     choice1: "var num = 5 + '5'",
-    //     choice2: "var num = 'fifty' + '5'",
-    //     choice3: "var num = null + 5",
-    //     choice4: "var num = 5 + undefined",
-    //     answer: "var num = 5 + undefined"
-    // },
-    // {
-    //     question: "Which of the following means 'strict equality'?",
-    //     choice1: "===",
-    //     choice2: "==",
-    //     choice3: "!==",
-    //     choice4: "=",
-    //     answer: "==="
-    // },
-    // {
-    //     question: "What will be returned when the following is logged in the console: [22, 24] === [22, 24]?",
-    //     choice1: "false",
-    //     choice2: "true",
-    //     choice3: "undefined",
-    //     choice4: "NaN",
-    //     answer: "false"
-    // },
-    // {
-    //     question: "Which of the following array method will add a value to the BEGINING of an array?",
-    //     choice1: ".shift()",
-    //     choice2: ".unshift()",
-    //     choice3: ".pop()",
-    //     choice4: ".push()",
-    //     answer: ".unshift()"
-    // }
+    {
+        question: "Which is the most efficient algorithm using 'Big O' notation?",
+        choice1: "O(N)",
+        choice2: "O(1)",
+        choice3: "O(LogN)",
+        choice4: "O(2N)",
+        answer: "O(1)"
+    },
+    {
+        question: "Which is the correct way to identify 'orange' in the following array: var fruits = ['apple', 'orange', 'pear];",
+        choice1: "fruits[1]",
+        choice2: "fruits[0]",
+        choice3: "fruits[2]",
+        choice4: "fruits[orange]",
+        answer: "fruits[1]"
+    },
+    {
+        question: "Which of the following is the right syntax for calling an element with the class name 'first",
+        choice1: "document.querySelector(.first)",
+        choice2: "document.querySelector('.first')",
+        choice3: "document.getClassName(.first)",
+        choice4: "document.getClassName('.first')",
+        answer: "document.querySelector('.first')"
+    },
+    {
+        question: "Which best describes JavaScript?",
+        choice1: "double-threaded",
+        choice2: "triple-threaded",
+        choice3: "non-threaded",
+        choice4: "single-threaded",
+        answer: "single-threaded"
+    },
+    {
+        question: "Which variable's value cannot be changed once declared?",
+        choice1: "const",
+        choice2: "let",
+        choice3: "var",
+        choice4: "span",
+        answer: "const"
+    },
+    {
+        question: "Which method extracts a portion of a string without altering the original and returns it as a new string?",
+        choice1: "trim()",
+        choice2: "replace()",
+        choice3: "slice()",
+        choice4: "splice()",
+        answer: "slice()"
+    },
+    {
+        question: "Which method removes the decimal from a number?",
+        choice1: "Math.random()",
+        choice2: "console.log()",
+        choice3: "Math.floor()",
+        choice4: "indexOf()",
+        answer: "Math.floor()"
+    },
+    {
+        question: "How many times will this loop run? for (let i = 0; i < 5; i++)",
+        choice1: "3",
+        choice2: "10",
+        choice3: "15",
+        choice4: "5",
+        answer: "5"
+    },
+    {
+        question: "Which of the following will create an infinite loop?",
+        choice1: "for (let i = 0; i < 6; i++)",
+        choice2: "for (let i = 0; i > 5; i++)",
+        choice3: "for (let i = 6; i > 5; i++)",
+        choice4: "for (let i = 4; i > 0; i--)",
+        answer: "for (let i = 6; i > 5; i++)"
+    },
+    {
+        question: "Which of the following is the conditon for the loop? for (let i = 0; i < 5; i++)",
+        choice1: "i < 5",
+        choice2: "let i = 0",
+        choice3: "i++",
+        choice4: "for",
+        answer: "i < 5"
+    },
+    {
+        question: "Which of the following is true?",
+        choice1: "5 === '5'",
+        choice2: "2 != '2'",
+        choice3: "true !== false",
+        choice4: "5 == 'five'",
+        answer: "2 != '2'"
+    },
+    {
+        question: "Which of the following is NOT correct syntax for writing a function?",
+        choice1: "function newFunc() { console.log('hello!) }",
+        choice2: "var newFunc = function() { console.log('hello!) }",
+        choice3: "var function() = newFunc { console.log('hello') }",
+        choice4: "var newFunc = () => { console.log('hello') }",
+        answer: "var function() = newFunc { console.log('hello') }"
+    },
+    {
+        question: "For the variable 'let x=5', which of the following will satisfy the conditions?",
+        choice1: "if (x > 1 && x < 4)",
+        choice2: "if (x > 1 || x < 4)",
+        choice3: "if (x < 1 || x < 4)",
+        choice4: "if (x > 5 && x < 10)",
+        answer: "if (x > 1 || x < 4)"
+    },
+    {
+        question: "Which of the following means 'Not A Number'?",
+        choice1: "undefined",
+        choice2: "NaN",
+        choice3: "null",
+        choice4: "false",
+        answer: "NaN"
+    },
+    {
+        question: "Which of the following will return NaN when called?",
+        choice1: "var num = 5 + '5'",
+        choice2: "var num = 'fifty' + '5'",
+        choice3: "var num = null + 5",
+        choice4: "var num = 5 + undefined",
+        answer: "var num = 5 + undefined"
+    },
+    {
+        question: "Which of the following means 'strict equality'?",
+        choice1: "===",
+        choice2: "==",
+        choice3: "!==",
+        choice4: "=",
+        answer: "==="
+    },
+    {
+        question: "What will be returned when the following is logged in the console: [22, 24] === [22, 24]?",
+        choice1: "false",
+        choice2: "true",
+        choice3: "undefined",
+        choice4: "NaN",
+        answer: "false"
+    },
+    {
+        question: "Which of the following array method will add a value to the BEGINING of an array?",
+        choice1: ".shift()",
+        choice2: ".unshift()",
+        choice3: ".pop()",
+        choice4: ".push()",
+        answer: ".unshift()"
+    }
 ];
 
 function playGame() {
